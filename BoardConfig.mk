ae46f7874d519c0edfee4c5168ac8a71ad256f1f
Jimmy Wu <jimmy.wu@daedalus.com.tw>
2020-01-31 03:38:09 +0800

    add 2nd commit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,18 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 
+#TARGET_ARCH := arm64
+#TARGET_ARCH_VARIANT := armv8-a
+#TARGET_CPU_ABI := arme64-v8a
+#TARGET_CPU_ABI2 := 
+#TARGET_CPU_VARIANT := generic
+
+#TARGET_2ND_ARCH := arm
+#TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+#TARGET_2ND_CPU_ABI := armeabi-v7a
+#TARGET_2ND_CPU_ABI2 := armeabi
+#TARGET_2ND_CPU_VARIANT := cortex-a53
+
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := zImage
 TARGET_KERNEL_CONFIG := lineageos_rpi4_defconfig
