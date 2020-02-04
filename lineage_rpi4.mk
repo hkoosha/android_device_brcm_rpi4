17fb494d25ac8ba59c5b3b8dfa29bfd9fa57aa06
Jimmy Wu <jimmy.wu@daedalus.com.tw>
2020-02-05 04:21:32 +0800

    change product name

--- lineage_rpi4.mk
+++ lineage_rpi4.mk
@@ -20,7 +20,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rpi4
 PRODUCT_NAME := lineage_rpi4
-PRODUCT_BRAND := Raspberry
-PRODUCT_MODEL := Raspberry Pi 4
-PRODUCT_MANUFACTURER := Raspberry
-PRODUCT_RELEASE_NAME := Raspberry Pi 4
+PRODUCT_BRAND := Richash
+PRODUCT_MODEL := Richash RPi4
+PRODUCT_MANUFACTURER := Richash
+PRODUCT_RELEASE_NAME := Richash RPi4
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.lineage.releasetype=Richash
